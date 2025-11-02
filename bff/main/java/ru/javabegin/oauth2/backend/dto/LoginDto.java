fc331029a7c228263bdda5230e73447374dc238e
bobijackrussel <andjela.milasinovic@student.flf.unibl.org>
2025-11-02 23:47:35 +0100

    Delete bff/main directory

--- bff/main/java/ru/javabegin/oauth2/backend/dto/LoginDto.java
+++
@@ -1,4 +0,0 @@
-package ru.javabegin.oauth2.backend.dto;
-
-public class LoginDto {
-}
--- bff/main/java/ru/javabegin/oauth2/backend/service/TurnstileService.java
+++
@@ -1,7 +0,0 @@
-package ru.javabegin.oauth2.backend.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class TurnstileService {
-}
